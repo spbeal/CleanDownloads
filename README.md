c6c09086275a13b78d565fbb43ebea77f56638e0
spbeal <62578556+spbeal@users.noreply.github.com>
2023-10-07 16:12:29 -0700

    Push

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CleanDesktop
- 
+
+Organizes download folder using python
+
+
+Base Design {
+-Track Desktop, if a file is added, then move.
+-Move files.
+-Run always in background.
+-System for file organization.
+    -Folders for file types
+    -- Folders organized by date
+-Need to find all file types to check what file has been added.
+}
---
+++ src/deskclean.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+from pathlib import Path
+from time import sleep
+
+from watchdog.observers import Observer
+
+from EventHandler import EventHandler
+
+if __name__ == '__main__':
+    watch_path = Path.home() / 'Downloads'
+    destination_root = Path.home() / 'Downloads/Storage'
+    event_handler = EventHandler(watch_path=watch_path, destination_root=destination_root)
+
+    observer = Observer()
+    observer.schedule(event_handler, f'{watch_path}', recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
