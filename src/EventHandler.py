183fab9d0e58a0b1e6fe6933bf0689377b3c3504
spbeal <62578556+spbeal@users.noreply.github.com>
2023-10-24 00:06:24 -0700

    Comments and reverse

--- src/EventHandler.py
+++ src/EventHandler.py
@@ -37,17 +37,17 @@ def rename_file(source: Path, destination_path: Path):
     else:
         return destination_path / source.name
 
-
 class EventHandler(FileSystemEventHandler):
     def __init__(self, watch_path: Path, destination_root: Path):
         self.watch_path = watch_path.resolve()
         self.destination_root = destination_root.resolve()
 
     def on_modified(self, event):
-        for child in self.watch_path.iterdir():
+        for child in self.watch_path.iterdir(): # Can change watch path to whatever
             # skips directories and non-specified extensions
             if child.is_file() and child.suffix.lower() in extension_paths:
-                destination_path = self.destination_root / extension_paths[child.suffix.lower()]
-                destination_path = add_date_to_path(path=destination_path)
-                destination_path = rename_file(source=child, destination_path=destination_path)
+                #destination_path = self.destination_root / extension_paths[child.suffix.lower()]
+                #destination_path = add_date_to_path(path=destination_path)
+                destination_path = self.destination_root  # uncomment to complete base case or move to specified areas
+                destination_path = rename_file(source=child, destination_path=destination_path) # accounts for duplicates
                 shutil.move(src=child, dst=destination_path)
--- src/deskclean.py
+++ src/deskclean.py
@@ -8,8 +8,8 @@ from watchdog.observers import Observer
 from EventHandler import EventHandler
 
 if __name__ == '__main__':
-    watch_path = Path.home() / 'Downloads'
-    destination_root = Path.home() / 'Downloads/Storage'
+    watch_path = Path.home() / 'Downloads' # change wherever you want to watch 
+    destination_root = Path.home() / 'Downloads/Storage' # wherever you want to put 
     event_handler = EventHandler(watch_path=watch_path, destination_root=destination_root)
 
     observer = Observer()
